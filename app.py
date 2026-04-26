81fce50b02b4e2ac50298781819d27c75d50ef6f
Jefferson <jefferson.iskandar@gmail.com>
2026-04-26 21:33:27 +0700

    remove matplotlib

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import os
 import streamlit.components.v1 as components
